b708008f68b1b56b5b3da4a1a08fd33173055e3d
Todd Treece <toddtreece@gmail.com>
2015-10-29 19:24:20 +0000

    bump aio version & use dotenvs in index.js

--- index.js
+++ index.js
@@ -2,64 +2,70 @@
 
 const Motion = require('motion').Stream,
       Camera = require('./lib/camera'),
-      AdafruitIO = require('adafruit-io'),
-      env = process.env;
+      AdafruitIO = require('adafruit-io');
 
-const aio = new AdafruitIO(
-  env.AIO_CLIENT_USER,
-  env.AIO_CLIENT_KEY,
-  {
-    host: env.AIO_CLIENT_HOST || 'io.adafruit.com',
-    port: env.AIO_CLIENT_PORT || 80,
-    success: ready,
-    failure: error
-  }
-);
+AdafruitIO.CLI.getConfigPath().then(path => {
 
-function ready() {
+  require('dotenv').config({silent: true, path: path});
 
-  const feed = aio.Feeds.writable(env.AIO_CAMFEED || 'picam'),
-        camera_options = {};
+  const env = process.env;
 
-  let camera = false;
+  const aio = new AdafruitIO(
+    env.AIO_CLIENT_USER,
+    env.AIO_CLIENT_KEY,
+    {
+      host: env.AIO_CLIENT_HOST || 'io.adafruit.com',
+      port: env.AIO_CLIENT_PORT || 80,
+      success: ready,
+      failure: error
+    }
+  );
 
-  if(env.CAM_VFLIP === 'true' || env.CAM_VFLIP === '1')
-    camera_options.vflip = true;
+  function ready() {
 
-  if(env.CAM_HFLIP === 'true' || env.CAM_HFLIP === '1')
-    camera_options.hflip = true;
+    const feed = aio.Feeds.writable(env.AIO_CAMFEED || 'picam'),
+          camera_options = {};
 
-  if(env.MOTION === 'true' || env.MOTION === '1') {
+    let camera = false;
 
-    const motion = new Motion({
-      threshold: env.MOTION_THRESH ? parseInt(env.MOTION_THRESH) : 0x15,
-      minChange: env.MOTION_MINCHANGE ? parseInt(env.MOTION_MINCHANGE) : 10,
-      minimumMotion: env.MOTION_MINSECONDS ? parseInt(env.MOTION_MINSECONDS) : 1,
-      prebuffer: 0,
-      postbuffer: 0
-    });
+    if(env.CAM_VFLIP === 'true' || env.CAM_VFLIP === '1')
+      camera_options.vflip = true;
 
-    camera_options.timelapse = env.CAM_RATE ? parseInt(env.CAM_RATE) * 1000 : 2000;
+    if(env.CAM_HFLIP === 'true' || env.CAM_HFLIP === '1')
+      camera_options.hflip = true;
+
+    if(env.MOTION === 'true' || env.MOTION === '1') {
+
+      const motion = new Motion({
+        threshold: env.MOTION_THRESH ? parseInt(env.MOTION_THRESH) : 0x15,
+        minChange: env.MOTION_MINCHANGE ? parseInt(env.MOTION_MINCHANGE) : 10,
+        minimumMotion: env.MOTION_MINSECONDS ? parseInt(env.MOTION_MINSECONDS) : 1,
+        prebuffer: 0,
+        postbuffer: 0
+      });
+
+      camera_options.timelapse = env.CAM_RATE ? parseInt(env.CAM_RATE) * 1000 : 2000;
+      camera = new Camera(camera_options);
+      camera.pipe(motion);
+
+      return motion.on('data', (img) => {
+        feed.write(img.toString('base64'));
+      });
+
+    }
+
+    camera_options.timelapse = env.CAM_RATE ? parseInt(env.CAM_RATE) * 1000 : 5000;
     camera = new Camera(camera_options);
-    camera.pipe(motion);
 
-    return motion.on('data', (img) => {
+    return camera.on('data', (img) => {
       feed.write(img.toString('base64'));
     });
 
   }
 
-  camera_options.timelapse = env.CAM_RATE ? parseInt(env.CAM_RATE) * 1000 : 5000;
-  camera = new Camera(camera_options);
+  function error(err) {
+    console.error(err);
+    process.exit(1);
+  }
 
-  return camera.on('data', (img) => {
-    feed.write(img.toString('base64'));
-  });
-
-}
-
-function error(err) {
-  console.error(err);
-  process.exit(1);
-}
-
+});
